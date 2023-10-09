f08cc5daeb4f06233aee342674538ba72edf7557
Thiago Teruel <thiteruel@gmail.com>
2023-10-09 11:54:39 -0300

    lorem

--- UrnaEletronica/UrnaEletronica.js
+++ UrnaEletronica/UrnaEletronica.js
@@ -1,31 +1,36 @@
 
 function urnaEletronica(){
-    const voto = parseInt(document.getElementById('voto').value);
-    var candidato1;
-    var candidato2;
-    var candidato3;
-    var votoBranco;
-    var votoNulo;
+    var voto;
+    
+    for(;voto != 0;){
+        const voto = parseInt(document.getElementById('voto').value);
+        var candidato1;
+        var candidato2;
+        var candidato3;
+        var votoBranco;
+        var votoNulo;
 
-    if (voto == 1){
-        window.alert("Você votou no candidato 1.")
-        candidato1++;
-    } else if (voto == 2){
-        window.alert("Você votou no candidato 2.")
-        candidato2++;
-    } else if (voto == 3){
-        window.alert("Você votou no candidato 3.")
-        candidato3++;
-    } else if (voto == 5){
-        window.alert("Você votou em branco.")
-        votoBranco++;
-    } else if (voto == 8){
-        window.alert("Você votou nulo.")
-        votoNulo++;
-    } else if{
-        window.alert("Numero invalido")
-    } else{
-        window.alert("Numero invalido")
+    
+        if (voto == 1){
+            window.alert("Você votou no candidato 1.")
+            candidato1++;
+        } else if (voto == 2){
+            window.alert("Você votou no candidato 2.")
+            candidato2++;
+        } else if (voto == 3){
+            window.alert("Você votou no candidato 3.")
+            candidato3++;
+        } else if (voto == 5){
+            window.alert("Você votou em branco.")
+            votoBranco++;
+        } else if (voto == 8){
+            window.alert("Você votou nulo.")
+            votoNulo++;
+        } else{
+            window.alert("Numero invalido")
+        }
     }
+
+    
 
 }
