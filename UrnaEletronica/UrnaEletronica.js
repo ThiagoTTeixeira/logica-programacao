7f60ac15c210ea10d746c87571225f7f65a057e3
Thiago Teruel <thiteruel@gmail.com>
2023-10-10 16:38:40 -0300

    Urna simples terminada

--- UrnaEletronica/UrnaEletronica.js
+++ UrnaEletronica/UrnaEletronica.js
@@ -1,36 +1,64 @@
 
 function urnaEletronica(){
-    var voto;
-    
-    for(;voto != 0;){
-        const voto = parseInt(document.getElementById('voto').value);
-        var candidato1;
-        var candidato2;
-        var candidato3;
-        var votoBranco;
-        var votoNulo;
+    var candidato1 = 0;
+    var candidato2 = 0;
+    var candidato3 = 0;
+    var votoBranco = 0;
+    var votoNulo = 0;
+    var porcentagem = 0;
+
+    for(var voto;voto != 0;){
+        voto = parseInt(prompt("Digite a opção"));
 
     
         if (voto == 1){
-            window.alert("Você votou no candidato 1.")
+            console.log("Voto computado para candidato 1.")
             candidato1++;
         } else if (voto == 2){
-            window.alert("Você votou no candidato 2.")
+            console.log("Voto computado para candidato 2.")
             candidato2++;
         } else if (voto == 3){
-            window.alert("Você votou no candidato 3.")
+            console.log("Voto computado para candidato 3.")
             candidato3++;
         } else if (voto == 5){
-            window.alert("Você votou em branco.")
+            console.log("Você computado para Voto em Branco.")
             votoBranco++;
         } else if (voto == 8){
-            window.alert("Você votou nulo.")
+            console.log("Você computado para Voto Nulo.")
             votoNulo++;
+        } else if(voto == 0){
+           alert("Votação encerrada")
         } else{
-            window.alert("Numero invalido")
+            alert("Numero invalido")
         }
-    }
 
-    
+    }
+    porcentagem = (candidato1 + candidato2 + candidato3 + votoBranco + votoNulo)/100
+    if (candidato1 > candidato2 && candidato1 > candidato3){
+        console.log("Ganhador foi Candidato 1 ", (candidato1/porcentagem))
+        console.log("Candidato 2 ", (candidato2/porcentagem))
+        console.log("Candidato 3 ", (candidato3/porcentagem))
+        console.log("Votos em Branco ", (votoBranco/porcentagem))
+        console.log("Votos nulo", (votoNulo/porcentagem))
+    } else  if (candidato2 > candidato1 && candidato2 > candidato3){
+        console.log("Ganhador foi Candidato 2 ", (candidato2/porcentagem))
+        console.log("Candidato 1 ", (candidato1/porcentagem))
+        console.log("Candidato 3 ", (candidato3/porcentagem))
+        console.log("Votos em Branco ", (votoBranco/porcentagem))
+        console.log("Votos nulo", (votoNulo/porcentagem))
+    } else  if (candidato3 > candidato2 && candidato3 > candidato1){
+        console.log("Ganhador foi Candidato 3 ", (candidato3/porcentagem))
+        console.log("Candidato 1 ", (candidato1/porcentagem))
+        console.log("Candidato 2 ", (candidato2/porcentagem))
+        console.log("Votos em Branco ", (votoBranco/porcentagem))
+        console.log("Votos nulo", (votoNulo/porcentagem))
+    }  else {
+        console.log("Empate! ") 
+        console.log("Cndidato 1 ", (candidato1/porcentagem))
+        console.log("Candidato 2 ", (candidato2/porcentagem))
+        console.log("Candidato 3 ", (candidato3/porcentagem))
+        console.log("Votos em Branco ", (votoBranco/porcentagem))
+        console.log("Votos nulo", (votoNulo/porcentagem))
+    }
 
 }
