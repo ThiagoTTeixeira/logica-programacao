24067a80d7830dcaab46356e017e65e502dd0120
Thiago Teruel <thiteruel@gmail.com>
2023-10-31 16:54:32 -0300

    feat: quase terminado

--- UrnaEletronica/UrnaEletronica.js
+++ UrnaEletronica/UrnaEletronica.js
@@ -4,13 +4,20 @@ function urnaEletronica(){
         [2, "Jasson"],
         [3, "Juliana"],
         [4, "Thiago"],
-        [5, "Hian"]
+        [5, "Hian"],
     ]
-    let votos=[]
+    let votos=[0]
     let voto
-    votoNulo = 0
-    votoBranco = 0
+    let votoNulo = 0
+    let votoBranco = 0
     let senha = 123
+    let v = 0
+    let dataFinal
+    let numCand
+    console.clear()
+    for (i = 0; i < 99; i++){
+            votos.push(0)
+    }
     let configCand = confirm("Você gostaria de configurar os candidatos?")
     if (configCand){
         config()
@@ -19,29 +26,27 @@ function urnaEletronica(){
     if(configCand == false){
         config()
     }
-    for (i = 0; i < 5; i++){
-        if (votos.length< candidatos[i][0]){
-            votos.push(0)
-        }
-    }
-    for(let s = true, v = 0; s == true; v++){
+    let dataInicio = new Date();
+    for(let s = false; s == false; v++){
         voto = parseInt(prompt("Digite sua opção"))
-        let dataInicio = new Date();
+        confirmaVoto = false
         if (voto == senha){
-            s = !confirm("Você gostaria de encerrar a votação?")
+            s = confirm("Você gostaria de encerrar a votação?")
             v--
-            if(s == false){
-                let dataFinal= new Date()
+            if(s == true){
+                dataFinal= new Date()
             }
         }else{
-            for (let w = false, i = 0, confirmaVoto = false; w == false; i++){
-                if(i > 4 && voto != candidatos[i][0] && voto != 69){
+            for (let w = false, i = 0, confirmaVoto = false; w != true; i++){
+                if(i > 2){
                     confirmaVoto = confirm("Você confirma o voto nulo?")
-                    if(confirmavoto){
+                    if(confirmaVoto){
                         votoNulo++
                     }else{
                         v--
                     }
+                    w = true
+                
                 }else if (voto == 69){
                     confirmaVoto = confirm("Você confirma o voto branco?")
                     if(confirmaVoto){
@@ -49,34 +54,28 @@ function urnaEletronica(){
                     }else{
                         v--
                     }
-
-                }else{
-                    for (y;candidatos[voto][1] != "" && y != true;){
-                        for(p = false, i=0; p != true; i++){
-                            if(voto == candidatos[i][0]){
-                                confirmaVoto = confirm("Você confirma o voto no candidato "+ candidatos[i][1]+"?")
-                                if (confirmaVoto){
-                                    votos[voto]++
-                                }else {
-                                    v--
-                                }
-                                p = true
-                            }
-                        }
-                    y = true        
-                    }
                     w = true
+                }else if(voto == candidatos[voto][0]){
+                            confirmaVoto = confirm("Você confirma o voto no candidato "+ candidatos[voto][1]+"?")
+                            if (confirmaVoto){
+                                votos[voto]++
+                            }else {
+                                v--
+                            }  
+                    w = true 
+                }else if(voto == candidatos[voto-1][0] && voto){
+                    confirmaVoto = confirm("Você confirma o voto no candidato "+ candidatos[voto-1][1]+"?")
+                    if (confirmaVoto){
+                        votos[voto-1]++
+                    }else {
+                        v--
+                    }  
+                    w = true 
                 }    
-                
+            }    
         }
     }
 
-
-    
-    
-    
-    
-    
     let porcentagem = v/100
     console.clear()
     console.log("\n")
@@ -84,20 +83,35 @@ function urnaEletronica(){
     console.log("Inicio da voração: "+ dataInicio)
     console.log("Termino da voração: "+ dataFinal)
     console.log("\n")  
-    console.log("TOTAL DE VOTOS: ", v)
-    for(var i = 0; i <= 99;i++){
-        var result = votos[i]
-        console.log(candidatos[i], " terminou com ", (result / porcentagem).toFixed(2), "% de votos")
-    }
+    console.log("TOTAL DE VOTOS: "+ v)
+    console.log("Total votos em branco "+ (votoBranco/porcentagem).toFixed(2)+"% de votos")
+    console.log("Total votos nulo "+ (votoNulo/porcentagem).toFixed(2)+"% de votos")
 
+    for(var i = 0; i <= 99;i++){
+        if (candidatos[i][1] != ""){
+            console.log(candidatos[i][1], " terminou com "+ (votos[i]/porcentagem).toFixed(2)+ "% de votos")
+        }
+    }
+    
     function config(){
+        for (i = 0; i < 99; i++){
+            candidatos[i] = [];
+            for(let j = 0; j < 2; j++){
+                if (j>0){
+                    candidatos[i][j] = ""
+                }else{
+                    candidatos[i][j] = 0
+                }
+            }
+        }
         if (configCand){
             for (i = 0; i < 5; i++){
-                candidatos[i][0] = prompt("Digite o n° do "+ (i + 1) +" candidato")
-                for(;candidatos[i][0] == 69 | candidatos[i][0] > 99;){
-                    candidatos[i][0] = prompt("´Número indisponivel, não podeser 69, e nem maior que 99. Digite outro")
+                numCand = prompt("Digite o n° do "+ (i + 1) +" candidato")
+                candidatos[numCand][0] = numCand
+                for(;candidatos[numCand][0] == 69 | candidatos[numCand][0] > 99;){
+                    candidatos[numCand][0] = prompt("Número indisponivel, não podeser 69, e nem maior que 99. Digite outro")
                 }
-                candidatos[i][1] = prompt("Digite o nome do "+ (i + 1) +" candidato")
+                candidatos[numCand][1] = prompt("Digite o nome do "+ (i + 1) +" candidato")
                 
             }
             return candidatos
@@ -108,5 +122,4 @@ function urnaEletronica(){
         }
         
     }
-}
-}
+}    
